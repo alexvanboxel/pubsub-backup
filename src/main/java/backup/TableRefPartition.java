d27a0411fb0806c13d51fad295008eda92de60c4
Fokko Driesprong <fokkodriesprong@godatadriven.com>
2018-03-27 10:40:16 +0200

    Update example to Beam 2.3
    - Simplify the dependencies
    - Explicitly set partition on DAY when creating the BigQuery table

--- src/main/java/backup/TableRefPartition.java
+++ src/main/java/backup/TableRefPartition.java
@@ -2,6 +2,8 @@ package backup;
 
 import com.google.api.services.bigquery.model.TableReference;
 import com.google.api.services.bigquery.model.TableRow;
+import com.google.api.services.bigquery.model.TimePartitioning;
+import com.google.common.base.MoreObjects;
 import org.apache.beam.sdk.io.gcp.bigquery.TableDestination;
 import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.values.ValueInSingleWindow;
@@ -15,27 +17,43 @@ public class TableRefPartition implements SerializableFunction<ValueInSingleWind
     private final String datasetId;
     private final String pattern;
     private final String table;
+    private final String partitionType;
 
     public static TableRefPartition perDay(String projectId, String datasetId, String tablePrefix) {
-        return new TableRefPartition(projectId, datasetId, "yyyyMMdd", tablePrefix + "$");
+        return new TableRefPartition(projectId, datasetId, "yyyyMMdd", tablePrefix + "$", "DAY");
     }
 
-    private TableRefPartition(String projectId, String datasetId, String pattern, String table) {
+    private TableRefPartition(String projectId, String datasetId, String pattern, String table, String partitionType) {
         this.projectId = projectId;
         this.datasetId = datasetId;
         this.pattern = pattern;
         this.table = table;
+        this.partitionType = partitionType;
     }
 
     @Override
     public TableDestination apply(ValueInSingleWindow<TableRow> input) {
-        DateTimeFormatter partition = DateTimeFormat.forPattern(pattern).withZoneUTC();
+        final DateTimeFormatter partition = DateTimeFormat.forPattern(pattern).withZoneUTC();
 
-        TableReference reference = new TableReference();
+        final TimePartitioning timePartitioning = new TimePartitioning();
+        timePartitioning.setType(this.partitionType);
+
+        final TableReference reference = new TableReference();
         reference.setProjectId(this.projectId);
         reference.setDatasetId(this.datasetId);
 
         reference.setTableId(table + input.getWindow().maxTimestamp().toString(partition));
-        return new TableDestination(reference, null);
+        return new TableDestination(reference, this.toString(), timePartitioning);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                          .add("projectId", projectId)
+                          .add("datasetId", datasetId)
+                          .add("pattern", pattern)
+                          .add("table", table)
+                          .add("partitionType", partitionType)
+                          .toString();
     }
 }
